9ca5150a49158bcf694f2deb507f4884b7358336
andyymor <61852990+andyymor@users.noreply.github.com>
2024-01-30 01:58:54 +0000

    Created models for characters and planets

--- src/models.py
+++ src/models.py
@@ -7,26 +7,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+    # def to_dict(self):
+    #     return {}
+
 class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String)
+    birth_year = Column(String)
+    height = Column(Integer)
+    mass = Column(Integer)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
+    class Planets(Base):
+        id = Column(Integer, primary_key=True)
+        name = Column(String)
+        gravity = Column(Integer)
+        population = Column(Integer)
+        terrain = Column(String)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
