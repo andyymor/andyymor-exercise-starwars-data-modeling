d99e4a70dc81ac9db4b4e2654e1bb892036556c9
andyymor <61852990+andyymor@users.noreply.github.com>
2024-02-01 18:00:03 +0000

    created relations on user model & creating currently the favorites model using the foreign keys

--- src/models.py
+++ src/models.py
@@ -1,9 +1,13 @@
 import os
 import sys
+from typing import List
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 Base = declarative_base()
 
@@ -27,20 +31,64 @@ Base = declarative_base()
 
     # def to_dict(self):
     #     return {}
+# INSERT INTO user (name, last_name) VALUES ('Bob', 'Ross');
+
+# user = User()
+# user.name = "Bob"
+# user.last_name = "Ross"
+
+# # Add the user to the database
+# db.session.add(user)
+
+# # Similar to the Git commit, what this does is save all the changes you have made
+# db.session.commit()
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    Favorite = relationship("favorite", backpopulates = "User")
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey("user.id"))
+    planet_id = Column(ForeignKey)
 
 class Person(Base):
+    __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     birth_year = Column(String)
     height = Column(Integer)
     mass = Column(Integer)
 
-    class Planets(Base):
-        id = Column(Integer, primary_key=True)
-        name = Column(String)
-        gravity = Column(Integer)
-        population = Column(Integer)
-        terrain = Column(String)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gravity = Column(Integer)
+    population = Column(Integer)
+    terrain = Column(String)
+
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("left_id", ForeignKey("left_table.id")),
+    Column("right_id", ForeignKey("right_table.id")),
+)
+
+
+class Parent(Base):
+    __tablename__ = "left_table"
+    id = Column(Integer, primary_key=True)
+    children = relationship("Child", secondary=association_table)
+
+
+class Child(Base):
+    __tablename__ = "right_table"
+    id = Column(Integer, primary_key=True)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
