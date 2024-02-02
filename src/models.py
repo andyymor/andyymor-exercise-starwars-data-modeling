d7843dee19c4a6e202f5ae34188f5a6cba8d0b2c
andyymor <61852990+andyymor@users.noreply.github.com>
2024-02-02 00:39:51 +0000

    fixed tables and  added relationships

--- src/models.py
+++ src/models.py
@@ -1,59 +1,81 @@
-import os
-import sys
-from typing import List
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+# import os
+# import sys
+# from typing import List
+# from sqlalchemy import Column, ForeignKey, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship
+# from sqlalchemy import create_engine
 
-Base = declarative_base()
+# from sqlalchemy.orm import Mapped
+# from sqlalchemy.orm import mapped_column
+
+# Base = declarative_base()
+
+
+# class User(Base):
+#     __tablename__ = 'user'
+#     id = Column(Integer, primary_key=True)
+#     username = Column(String(64), unique=True, nullable=False)
+#     password = Column(String, nullable=False)
+#     Favorite = relationship("favorite", back_populates = "User")
+
+# class Favorite(Base):
+#     __tablename__ = 'favorite'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(ForeignKey("user.id"))
+#     planet_id = Column(ForeignKey)("planets.id")
+#     character_id = Column(ForeignKey)("person.id")
+#     user = relationship("User", back_populates = "Favorite")
+#     planet = relationship("Planets", back_populates = "Favorite")
+
 
 # class Person(Base):
 #     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
 #     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
+#     Favorites = relationship("favorites", back_populates="person") 
+#     name = Column(String)
+#     birth_year = Column(String)
+#     height = Column(Integer)
+#     mass = Column(Integer)
 
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
+
+# class Planets(Base):
+#     __tablename__ = 'planets'
 #     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
+#     Favorites = relationship("favorites", back_populates="planets")
+#     name = Column(String)
+#     gravity = Column(Integer)
+#     population = Column(Integer)
+#     terrain = Column(String)
 
-    # def to_dict(self):
-    #     return {}
-# INSERT INTO user (name, last_name) VALUES ('Bob', 'Ross');
 
-# user = User()
-# user.name = "Bob"
-# user.last_name = "Ross"
 
-# # Add the user to the database
-# db.session.add(user)
 
-# # Similar to the Git commit, what this does is save all the changes you have made
-# db.session.commit()
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+# from eralchemy2 import render_er
+
+Base = declarative_base()
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(64), unique=True, nullable=False)
     password = Column(String, nullable=False)
-    Favorite = relationship("favorite", backpopulates = "User")
+    favorites = relationship("Favorite", back_populates="user")
 
 class Favorite(Base):
     __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
-    user_id = Column(ForeignKey("user.id"))
-    planet_id = Column(ForeignKey)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    planet_id = Column(Integer, ForeignKey("planets.id"))
+    character_id = Column(Integer, ForeignKey("person.id"))
+    user = relationship("User", back_populates="favorites")
+    planet = relationship("Planets", back_populates="favorites")
+    character = relationship("Person", back_populates="favorites")
 
 class Person(Base):
     __tablename__ = 'person'
@@ -62,33 +84,18 @@ class Person(Base):
     birth_year = Column(String)
     height = Column(Integer)
     mass = Column(Integer)
-
+    favorites = relationship("Favorite", back_populates="character")
 
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    gravity = Column(Integer)
+    gravity = Column(String)  # Changed from Integer to String to accommodate values like '1 standard'
     population = Column(Integer)
     terrain = Column(String)
+    favorites = relationship("Favorite", back_populates="planet")
 
-association_table = Table(
-    "association_table",
-    Base.metadata,
-    Column("left_id", ForeignKey("left_table.id")),
-    Column("right_id", ForeignKey("right_table.id")),
-)
-
-
-class Parent(Base):
-    __tablename__ = "left_table"
-    id = Column(Integer, primary_key=True)
-    children = relationship("Child", secondary=association_table)
-
-
-class Child(Base):
-    __tablename__ = "right_table"
-    id = Column(Integer, primary_key=True)
-
+# Drawing the ER diagram would require having eralchemy installed and properly configured in your environment.
+# render_er(Base, 'diagram.png')
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+# render_er(Base, 'diagram.png')
